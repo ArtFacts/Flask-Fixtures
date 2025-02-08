e2d93642839dd03e950c411ae752c31edf9b597a
ulrich berthold <u@sansculotte.net>
2025-02-08 15:47:19 +0100

    call setUpClass with class as first argument

--- flask_fixtures/__init__.py
+++ flask_fixtures/__init__.py
@@ -1,14 +1,15 @@
 """
-    flask_fixtures
-    ~~~~~~~~~~~~~~~~~~
+flask_fixtures
+~~~~~~~~~~~~~~~~~~
 
-    Flask-Fixtures is a `Flask <http://flask.pocoo.org>`_ extension that aids
-    in the creation of fixtures data from serialized data files. It is
-    compatible with the `SQLAlchemy <http://sqlalchemy.org>`_ library.
+Flask-Fixtures is a `Flask <http://flask.pocoo.org>`_ extension that aids
+in the creation of fixtures data from serialized data files. It is
+compatible with the `SQLAlchemy <http://sqlalchemy.org>`_ library.
 
-    :copyright: (c) 2015 Christopher Roach <ask.croach@gmail.com>.
-    :license: MIT, see LICENSE for more details.
+:copyright: (c) 2015 Christopher Roach <ask.croach@gmail.com>.
+:license: MIT, see LICENSE for more details.
 """
+
 from __future__ import absolute_import
 
 import logging
@@ -22,18 +23,13 @@ import six
 import importlib
 
 from flask import current_app
-from flask import _request_ctx_stack
-try:
-    from flask import _app_ctx_stack
-except ImportError:
-    _app_ctx_stack = None
 
 try:
     import simplejson as json
 except ImportError:
     import json
 
-__version__ = '0.4.1'
+__version__ = "0.4.1"
 
 
 # Configure the root logger for the library
@@ -42,14 +38,28 @@ logging.basicConfig(format=logger_format_string, level=logging.INFO)
 log = logging.getLogger(__name__)
 
 
-CLASS_SETUP_NAMES = ('setUpClass', 'setup_class', 'setup_all', 'setupClass', 'setupAll', 'setUpAll')
-CLASS_TEARDOWN_NAMES = ('tearDownClass', 'teardown_class', 'teardown_all', 'teardownClass', 'teardownAll', 'tearDownAll')
-TEST_SETUP_NAMES = ('setUp',)
-TEST_TEARDOWN_NAMES = ('tearDown',)
+CLASS_SETUP_NAMES = (
+    "setUpClass",
+    "setup_class",
+    "setup_all",
+    "setupClass",
+    "setupAll",
+    "setUpAll",
+)
+CLASS_TEARDOWN_NAMES = (
+    "tearDownClass",
+    "teardown_class",
+    "teardown_all",
+    "teardownClass",
+    "teardownAll",
+    "tearDownAll",
+)
+TEST_SETUP_NAMES = ("setUp",)
+TEST_TEARDOWN_NAMES = ("tearDown",)
 
 
 def setup(obj):
-    log.info('setting up fixtures...')
+    log.info("setting up fixtures...")
 
     with obj.app.app_context():
         # Setup the database
@@ -62,11 +72,11 @@ def setup(obj):
 
 def setup_fixtures(obj):
     # Construct a list of paths within which fixtures may reside
-    default_fixtures_dir = os.path.join(current_app.root_path, 'fixtures')
+    default_fixtures_dir = os.path.join(current_app.root_path, "fixtures")
 
     # All relative paths should be relative to the app's root directory.
     fixtures_dirs = [default_fixtures_dir]
-    for directory in current_app.config.get('FIXTURES_DIRS', []):
+    for directory in current_app.config.get("FIXTURES_DIRS", []):
         if not os.path.isabs(directory):
             directory = os.path.abspath(os.path.join(current_app.root_path, directory))
         fixtures_dirs.append(directory)
@@ -80,11 +90,13 @@ def setup_fixtures(obj):
                 load_fixtures(obj.db, loaders.load(filepath))
                 break
         else:
-            raise IOError("Error loading '{0}'. File could not be found".format(filename))
+            raise IOError(
+                "Error loading '{0}'. File could not be found".format(filename)
+            )
 
 
 def teardown(obj):
-    log.info('tearing down fixtures...')
+    log.info("tearing down fixtures...")
     with obj.app.app_context():
         obj.db.session.expunge_all()
         obj.db.session.close()
@@ -92,35 +104,39 @@ def teardown(obj):
 
 
 def load_fixtures(db, fixtures):
-    """Loads the given fixtures into the database.
-    """
+    """Loads the given fixtures into the database."""
     conn = db.engine.connect()
     metadata = db.metadata
 
     for fixture in fixtures:
-        if 'model' in fixture:
-            module_name, class_name = fixture['model'].rsplit('.', 1)
+        if "model" in fixture:
+            module_name, class_name = fixture["model"].rsplit(".", 1)
             module = importlib.import_module(module_name)
             model = getattr(module, class_name)
-            for fields in fixture['records']:
+            for fields in fixture["records"]:
                 obj = model(**fields)
                 db.session.add(obj)
             db.session.commit()
-        elif 'table' in fixture:
-            table = Table(fixture['table'], metadata)
-            conn.execute(table.insert(), fixture['records'])
+        elif "table" in fixture:
+            table = Table(fixture["table"], metadata)
+            conn.execute(table.insert(), fixture["records"])
         else:
-            raise ValueError("Fixture missing a 'model' or 'table' field: {0}".format(json.dumps(fixture)))
+            raise ValueError(
+                "Fixture missing a 'model' or 'table' field: {0}".format(
+                    json.dumps(fixture)
+                )
+            )
 
 
 class MetaFixturesMixin(type):
     def __new__(meta, name, bases, attrs):
-
-        fixtures = attrs.get('fixtures', [])
+        fixtures = attrs.get("fixtures", [])
 
         # Should we persist fixtures across tests, i.e., should we use the
         # setUpClass and tearDownClass methods instead of setUp and tearDown?
-        persist_fixtures = attrs.get('persist_fixtures', False) and can_persist_fixtures()
+        persist_fixtures = (
+            attrs.get("persist_fixtures", False) and can_persist_fixtures()
+        )
 
         # We only need to do something if there's a set of fixtures,
         # otherwise, do nothing. The main reason this is here is because this
@@ -130,13 +146,23 @@ class MetaFixturesMixin(type):
             if not persist_fixtures:
                 child_setup_fn = meta.get_child_fn(attrs, TEST_SETUP_NAMES, bases)
                 child_teardown_fn = meta.get_child_fn(attrs, TEST_TEARDOWN_NAMES, bases)
-                attrs[child_setup_fn.__name__] = meta.setup_handler(setup, child_setup_fn)
-                attrs[child_teardown_fn.__name__] = meta.teardown_handler(teardown, child_teardown_fn)
+                attrs[child_setup_fn.__name__] = meta.setup_handler(
+                    setup, child_setup_fn
+                )
+                attrs[child_teardown_fn.__name__] = meta.teardown_handler(
+                    teardown, child_teardown_fn
+                )
             else:
                 child_setup_fn = meta.get_child_fn(attrs, CLASS_SETUP_NAMES, bases)
-                child_teardown_fn = meta.get_child_fn(attrs, CLASS_TEARDOWN_NAMES, bases)
-                attrs[child_setup_fn.__name__] = classmethod(meta.setup_handler(setup, child_setup_fn))
-                attrs[child_teardown_fn.__name__] = classmethod(meta.teardown_handler(teardown, child_teardown_fn))
+                child_teardown_fn = meta.get_child_fn(
+                    attrs, CLASS_TEARDOWN_NAMES, bases
+                )
+                attrs[child_setup_fn.__name__] = classmethod(
+                    meta.setup_handler(setup, child_setup_fn)
+                )
+                attrs[child_teardown_fn.__name__] = classmethod(
+                    meta.teardown_handler(teardown, child_teardown_fn)
+                )
 
         return super(MetaFixturesMixin, meta).__new__(meta, name, bases, attrs)
 
@@ -146,9 +172,11 @@ class MetaFixturesMixin(type):
 
         Makes sure that fixtures are setup before calling the given setup method.
         """
+
         def handler(obj):
             setup_fixtures_fn(obj)
             setup_fn(obj)
+
         return handler
 
     @staticmethod
@@ -157,9 +185,11 @@ class MetaFixturesMixin(type):
 
         Calls the given teardown method first before calling the fixtures teardown.
         """
+
         def handler(obj):
             teardown_fn(obj)
             teardown_fixtures_fn(obj)
+
         return handler
 
     @staticmethod
@@ -173,9 +203,9 @@ class MetaFixturesMixin(type):
         returned.
 
         """
+
         def call_method(obj, method):
-            """Calls a method as either a class method or an instance method.
-            """
+            """Calls a method as either a class method or an instance method."""
             # The __get__ method takes an instance and an owner which changes
             # depending on the calling object. If the calling object is a class,
             # the instance is None and the owner will be the object itself. If the
@@ -185,17 +215,20 @@ class MetaFixturesMixin(type):
             if isinstance(obj, type):
                 instance = None
                 owner = obj
+                method.__get__(instance, owner)(obj)
             else:
                 instance = obj
                 owner = obj.__class__
-            method.__get__(instance, owner)()
+                method.__get__(instance, owner)()
 
         # Create a default function that calls the default method on each parent
         default_name = names[0]
+
         def default_fn(obj):
             for cls in bases:
                 if hasattr(cls, default_name):
                     call_method(obj, getattr(cls, default_name))
+
         default_fn.__name__ = default_name
 
         # Get all of the functions in the child class that match the list of names
@@ -203,12 +236,16 @@ class MetaFixturesMixin(type):
 
         # Raise an error if more than one setup/teardown method is found
         if len(fns) > 1:
-            raise RuntimeError("Cannot have more than one setup or teardown method per context (class or test).")
+            raise RuntimeError(
+                "Cannot have more than one setup or teardown method per context (class or test)."
+            )
         # If one setup/teardown function was found, return it
         elif len(fns) == 1:
             name, fn = fns[0]
+
             def child_fn(obj):
                 call_method(obj, fn)
+
             child_fn.__name__ = name
             return child_fn
         # Otherwise, return the default function
@@ -217,8 +254,6 @@ class MetaFixturesMixin(type):
 
 
 class FixturesMixin(six.with_metaclass(MetaFixturesMixin, object)):
-
     fixtures = None
     app = None
     db = None
-
